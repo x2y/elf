e05066a15f6507956b59a8eed6b4cf7ca8ef38e6
0x24a537r9 <0x24a537r9@gmail.com>
2014-07-22 00:52:10 -0700

    Defines the basic models.

--- models.py
+++ models.py
@@ -1,7 +1,54 @@
-import base64
-import datetime
-import math
-import random
-import struct
+import re
 
+from email.utils import parseaddr
 from google.appengine.ext import ndb
+
+
+NAME_KEY = 'name'
+EMAIL_KEY = 'email'
+GIVER_EMAIL_KEY = 'giverEmail'
+RECEIVER_EMAIL_KEY = 'receiverEmail'
+USERS_KEY = 'users'
+POSITIVE_CONSTRAINTS_KEY = 'positiveConstraints'
+NEGATIVE_CONSTRAINTS_KEY = 'negativeConstraints'
+ASSIGNMENTS_KEY = 'assignments'
+
+
+def validate_email(email):
+    if not re.match(r'[^@]+@[^@]+\.[^@]+', email):
+        raise Exception('Invalid email: %s' % email)
+
+
+class User(ndb.Model):
+    name = ndb.StringProperty(required=True, indexed=False)
+    email = ndb.StringProperty(required=True, indexed=False, validator=validate_email)
+
+    def to_dict(self):
+        return {
+            NAME_KEY: self.name,
+            EMAIL_KEY: self.email
+        }
+
+
+class Assignment(ndb.Model):
+    giver_email = ndb.StringProperty(required=True, indexed=False, validator=validate_email)
+    receiver_email = ndb.StringProperty(required=True, indexed=False, validator=validate_email)
+
+    def to_dict(self):
+        return {
+            GIVER_EMAIL_KEY: self.giver_email,
+            RECEIVER_EMAIL_KEY: self.receiver_email,
+        }
+
+
+class Party(ndb.Model):
+    users = ndb.StructuredProperty(User, repeated=True)
+    positive_constraints = ndb.StructuredProperty(Assignment, repeated=True)
+    negative_constraints = ndb.StructuredProperty(Assignment, repeated=True)
+    assignments = ndb.StructuredProperty(Assignment, repeated=True)
+
+    def to_dict(self):
+        return {
+            USERS_KEY: [user.to_dict() for user in self.users],
+            ASSIGNMENTS_KEY: [assignment.to_dict() for assignment in self.assignments],
+        }
